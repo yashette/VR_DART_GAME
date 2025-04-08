61494a986bca9047227276c65ddd969f2c41811f
yashette <79081473+yashette@users.noreply.github.com>
2025-04-08 22:19:54 +0200

    'game_menu_and_setting_menu'

--- Assets/GameMenuManager.cs
+++ Assets/GameMenuManager.cs
@@ -5,30 +5,34 @@ using UnityEngine.InputSystem;
 
 public class GameMenuManager : MonoBehaviour
 {
-    public Transform head;
-    public float distance = 2;
-    public GameObject gameMenu;
-    public InputActionProperty showButton;
+    public Transform head;                        // Caméra ou tête du joueur
+    public float distance = 2f;                   // Distance devant le joueur
+    public float fixedHeight = 4.65f;             // Hauteur fixe pour le menu
+    public GameObject gameMenu;                   // GameObject parent du menu
+    public InputActionProperty showButton;        // Bouton d'affichage
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     void Update()
     {
-
-        if(showButton.action.WasPressedThisFrame())
+        if (showButton.action.WasPressedThisFrame())
         {
             gameMenu.SetActive(!gameMenu.activeSelf);
 
-            gameMenu.transform.position = head.position + new Vector3(head.forward.x, head.forward.z).normalized * distance;
+            if (gameMenu.activeSelf)
+            {
+                // Direction devant la tête (horizontale uniquement)
+                Vector3 forward = new Vector3(head.forward.x, 0, head.forward.z).normalized;
+
+                // Position à une hauteur fixe (y = 4.65)
+                Vector3 spawnPosition = head.position + forward * distance;
+                spawnPosition.y = fixedHeight;
+
+                gameMenu.transform.position = spawnPosition;
+
+                // Regarder la tête du joueur (en gardant la hauteur fixe)
+                Vector3 lookAtTarget = new Vector3(head.position.x, fixedHeight, head.position.z);
+                gameMenu.transform.LookAt(lookAtTarget);
+                gameMenu.transform.Rotate(0, 180f, 0); // Retourner pour que le menu soit lisible
+            }
         }
-
-        gameMenu.transform.LookAt(new Vector3(head.forward.x, gameMenu.transform.position.y, head.position.z));
-        gameMenu.transform.forward *= -1;   
-
     }
 }
