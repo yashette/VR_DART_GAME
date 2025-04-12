c3523dd189583ea0daa6f6a6ddde17b4a61a756a
yashette <79081473+yashette@users.noreply.github.com>
2025-04-12 19:26:58 +0200

    'gamemode_and_reset_collider_bug'

--- Assets/ScoreManager.cs
+++ Assets/ScoreManager.cs
@@ -4,29 +4,37 @@ using UnityEngine.UI;
 
 public class ScoreManager : MonoBehaviour
 {
-    public int score = 301; // Score initial
+    public int currentScore;
+    public int initialScore;
     public TMP_Text scoreText;  // Référence au texte UI
 
     void Start()
     {
+        SetPoints(301);
         UpdateScoreUI();
     }
 
     public void SoustrairePoints(int points)
     {
-        int tempscore = score; // Stocker le score avant la soustraction
+        int tempscore = currentScore; // Stocker le score avant la soustraction
 
-        score -= points;
-        if (score < 0)
-            score = tempscore;
+        currentScore -= points;
+        if (currentScore < 0)
+            currentScore = tempscore;
 
         UpdateScoreUI();
     }
 
     public void SetPoints(int points)
     {
-            score = points;
+        currentScore = points;
+        initialScore = points;
 
+        UpdateScoreUI();
+    }
+    public void ResetScore()
+    {
+        currentScore = initialScore;     // <-- remet à zéro avec la bonne valeur
         UpdateScoreUI();
     }
 
@@ -34,7 +42,7 @@ public class ScoreManager : MonoBehaviour
     {
         if (scoreText != null)
         {
-            scoreText.text = "Score: " + score.ToString();
+            scoreText.text = "Score: " + currentScore.ToString();
         }
     }
 }
